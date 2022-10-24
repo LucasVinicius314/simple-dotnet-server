29c92e8ef35e702a0e5bcc8c9e107ba5dc33e26e
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-10-23 23:19:56 -0300

    ssl

--- App/Program.cs
+++ App/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Authentication.Certificate;
+
 var builder = WebApplication.CreateBuilder(args);
 
 if (Utils.IsProductionServer())
 {
   builder.WebHost.UseUrls("https://localhost:443");
+
+  builder.Services.AddAuthentication(
+        CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate();
 }
 
 var app = builder.Build();
+
+if (Utils.IsProductionServer())
+{
+  app.UseAuthentication();
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
